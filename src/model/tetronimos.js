f125863540b53d08ad87dd3528b782f342b1917d
Loïc Piernas <lpiernas.pers@gmail.com>
2023-01-06 15:23:20 +0100

    Add precheck in rotate90 Tetronimos

--- src/model/tetronimos.js
+++ src/model/tetronimos.js
@@ -1,30 +1,38 @@
 // This file contains data structure of game element
 import {CELL_SIZE} from './settings.js';
-function Tetronimos(array = [], color = 'green', x = 0, y = 0) {
-    this.x = x;
-    this.y = y;
-    this.color = color;
-    this.repArray = array;
-    this.rotate90 = function(nbRot = 1) {
-        // Prerequisite: rotation is possible 
-        // Rptate 90 clockwise
-        for (let i = 0; i < nbRot; i++) {
-            newArray = this.repArray[0].map((val, index) => this.repArray.map(row => row[index]).reverse());
-        }
-    }
-    this.draw = function(context) {
-        // Draw the tetronimos to the canvas
-        for (let i = 0; i < this.repArray.length; i++) {
-            for (let j = 0; j < this.repArray[i].length; j++) {
-                if (this.repArray[i][j] === 1) {
-                    context.fillStyle = this.color;
-                    context.fillRect(this.x + j * CELL_SIZE, this.y + i * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+class Tetronimos{
+    constructor(array = [], color = 'green', x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.repArray = array;
+        this.rotate90 = function(grid, nbRot = 1) {
+            // Rotate 90 clockwise
+            // Raise error if rotation is not possible
+            for (let i = 0; i < nbRot; i++) {
+                newArray = this.repArray[0].map((val, index) => this.repArray.map(row => row[index]).reverse());
+            }
+            for (let i = 0; i < newArray.length; i++) {
+                for (let j = 0; j < newArray[i].length; j++) {
+                    if (newArray[i][j] === 1 && grid[this.x + j][this.y + i] === 1) {
+                        throw new Error('Rotation not possible');
+                    }
                 }
             }
-        } 
-    }
-    
-        
+        }
+        this.draw = function(context) {
+            // Prerequisite: rotation is possible
+            // Draw the tetronimos to the canvas
+            for (let i = 0; i < this.repArray.length; i++) {
+                for (let j = 0; j < this.repArray[i].length; j++) {
+                    if (this.repArray[i][j] === 1) {
+                        context.fillStyle = this.color;
+                        context.fillRect(this.x + j * CELL_SIZE, this.y + i * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+                    }
+                }
+            } 
+        }
+    }   
 
 }
     
