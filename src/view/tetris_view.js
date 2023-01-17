656721ab01d2eb4ce1666e41bdf7073541aa85ea
Loïc Piernas <lpiernas.pers@gmail.com>
2023-01-17 09:59:00 +0100

    Neon mode !

--- src/view/tetris_view.js
+++ src/view/tetris_view.js
@@ -1,4 +1,4 @@
-import { CELL_SIZE } from "./../model/settings.js";
+import { CELL_SIZE, GRID_HEIGHT, GRID_WIDTH } from "./../model/settings.js";
 
 
 
@@ -21,16 +21,22 @@ export class TetrisView extends Object {
 
 
     drawCell(canvasContext, x, y, color) {
-        // Draw with canva
-        if (color != 0) color = color;
-        else color = "black";
-        canvasContext.fillStyle = color;
-        canvasContext.fillRect(x*this.#cellSize, y*this.#cellSize, this.#cellSize, this.#cellSize);
+        // Draw the border
+        if (color != 0){
+            canvasContext.strokeStyle = color;
+            // neon effect
+            canvasContext.shadowBlur = 10;
+            canvasContext.shadowColor = color;
+            canvasContext.lineWidth = 2;
+            canvasContext.strokeRect(x * this.#cellSize, y * this.#cellSize, this.#cellSize, this.#cellSize);
+        } 
 
     }
 
     drawGrid(canvasContext, grid){
         // Draw with canva
+        canvasContext.fillStyle = "black";
+        canvasContext.fillRect(0, 0, CELL_SIZE*GRID_WIDTH, CELL_SIZE*GRID_HEIGHT);
         for (let i = 0; i < grid.length; i++) {
             for (let j = 0; j < grid[i].length; j++) {
                 // Draw the cell
